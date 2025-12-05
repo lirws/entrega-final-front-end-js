2fb80cc74616997761650457b68f89b7465254d9
Andres <andeslires@gmail.com>
2025-12-05 06:20:52 -0300

    mensaje

---
+++ js/productos.js
@@ -0,0 +1,7 @@
+// detalle: rutas ABSOLUTAS ya que ahora se usaran desde 2 html en distintas ubicaciones
+export const productos = [
+    { id: "1", nombre: "Modelos pesado", img: "img/motos pesadas y grandes.jpg", precio: 699},
+    { id: "2", nombre: "Modelos livianos", img: "img/motos livianas.jpg", precio: 499},
+    { id: "3", nombre: "Modelos antiguos", img: "img/modelos antiguos.jpg", precio: 559},
+    { id: "4", nombre: "Última adquisición: Vespas", img: "img/vespa modelo.jpg", precio: 559},
+];
